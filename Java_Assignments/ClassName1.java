36aa0d370e00c9a67049ce38d6fb05e65c1edda8
G-Rajaraman <drivenraja@gmail.com>
2024-03-11 23:25:27 +0530

    Assignment 1

---
+++ Java_Assignments/ClassName1.java
@@ -0,0 +1,15 @@
+package basicprogram;
+public class ClassName1 
+{
+	public static void main(String[] args)//main method syntax
+	{
+	precondition();
+	System.out.println("Java main method");
+	}
+	
+	static void precondition()
+	{
+	System.out.println("Call from main method");
+	}
+
+}
